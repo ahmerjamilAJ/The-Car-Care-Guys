fa859a7ac785635805b8a1f799ae7896f1b3f62f
Asad-Soomro <63003674+Asad-Soomro@users.noreply.github.com>
2021-12-14 22:47:18 +0500

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
+const { post, put } = require('.');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { title: 'User' });
 });
+
+router.delete('/delete', function(req, res, next) {
+  console.log("body" , req.body)
+      connection.query('Delete from users where Id=?',[req.body.tid], function (error, results, fields) {
+      if (error) throw error;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+         
+  });
 
 module.exports = router;
