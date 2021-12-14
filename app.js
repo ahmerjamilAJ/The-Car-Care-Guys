8272af78675b72d48bdf697870759c7bba8da983
ahmerjamilAJ <86021055+ahmerjamilAJ@users.noreply.github.com>
2021-12-14 22:11:19 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ var signupRouter = require('./routes/signup');
 var signinRouter = require('./routes/signin');
 var adminsigninRouter = require('./routes/adminlogin');
 var signinworkerRouter = require('./routes/signinworker');
+
+var workersRouter = require('./routes/workers');
+var addworkersRouter = require('./routes/addworker');
+var servicesRouter = require('./routes/services');
+var packageRouter = require('./routes/packages');
+var reviewRouter = require('./routes/reviews');
+
+var userpanelRouter = require('./routes/userpanel');
+var bookingsRouter = require('./routes/bookings');
+var userpackageRouter = require('./routes/userpackages');
+
 var app = express();
 var mysql=require("mysql");
 
@@ -26,27 +37,61 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
 
 
-app.use(function(req,res,next){
-  res.locals.connection=mysql.createConnection({
-    host:'us-cdbr-east-04.cleardb.com',
+// app.use(function(req,res,next){
+//   res.locals.connection=mysql.createConnection({
+//     host:'us-cdbr-east-04.cleardb.com',
+//     user:'b080989610da46',
+//     password:'eb1e0f74',
+//     database:'heroku_678b68b3c4ffbc5',
+//     connectTimeout: 30000,
+//     acquireTimeout: 30000,
+//     connectionLimit: 16000
+
+//   });
+//     res.locals.connection.connect(); 
+//     next();
+
+    
+// });
+// mysql://b080989610da46:eb1e0f74@us-cdbr-east-04.cleardb.com/heroku_678b68b3c4ffbc5?reconnect=true
+
+
+var dbConfig = {
+  host:'us-cdbr-east-04.cleardb.com',
     user:'b080989610da46',
     password:'eb1e0f74',
     database:'heroku_678b68b3c4ffbc5',
     connectTimeout: 30000,
     acquireTimeout: 30000,
-    connectionLimit: 16000
+    connectionLimit: 16000,
+  port: 3306
+};
 
-  });
-    res.locals.connection.connect(); 
-    next();
-
-    
+var connection;
+function handleDisconnect() {
+connection = mysql.createConnection(dbConfig);  // Recreate the connection, since the old one cannot be reused.
+connection.connect( function onConnect(err) {   // The server is either down
+  if (err) {                                  // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 10000);    // We introduce a delay before attempting to reconnect,
+  }  
+  global.connection = connection                                         // to avoid a hot loop, and to allow our node script to
+});                                             // process asynchronous requests in the meantime.
+                                              // If you're also serving http, display a 503 error.
+connection.on('error', function onError(err) {
+  console.log('db error', err);
+  if (err.code == 'PROTOCOL_CONNECTION_LOST') {   // Connection to the MySQL server is usually
+      handleDisconnect();    
+      global.connection = connection                     // lost due to either server restart, or a
+  } else {                                        // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+  }
 });
-// mysql://b080989610da46:eb1e0f74@us-cdbr-east-04.cleardb.com/heroku_678b68b3c4ffbc5?reconnect=true
-
+}
+handleDisconnect();
 
 app.use(
   session({
@@ -60,8 +105,42 @@ app.use(
 
 
 app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+app.use('/signup', signupRouter);
+app.use('/signin', signinRouter);
+app.use('/admin', adminsigninRouter);
+app.use('/signinworker', signinworkerRouter);
 
+app.use('/workers', workersRouter);
+app.use('/addworkers', addworkersRouter);
+app.use('/services', servicesRouter);
+app.use('/packages', packageRouter);
+app.use('/reviews', reviewRouter);
+
+app.use('/userpanel', userpanelRouter);
+app.use('/booking', bookingsRouter);
+app.use('/userpackage', userpackageRouter);
+
+app.use('/admindashboard', function(req, res, next) {
+  res.render('index', { title: 'Type' });
+});
+
+app.use('/workerportal', function(req, res, next) {
+  res.render('workerportal', { title: 'Worker portal' });
+});
+
+app.use('/editworker', function(req, res, next) {
+  res.render('editworker', { title: 'Workers' });
+});
+
+app.use('/getservices', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+app.use('/getbookings', function(req, res, next) {
+  res.render('bookings', { title: 'Bookings' });
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
